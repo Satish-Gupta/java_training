288fd07404ad3ca752718410e0b8f1e39cfa3ceb
Satish <satis.kumar.gupta@gmail.com>
2014-12-30 02:20:26 +0545

    display sum of numbers between 100 and 200 and divisible by seven

---
+++ java_training/src/dec29/DivisibleBySeven.java
@@ -0,0 +1,26 @@
+/**
+ * find the number of and sum of all integers greater than 100 and less than 200 that are divisible by 7.
+ */
+package dec29;
+
+
+/**
+ * @author satish
+ *
+ */
+public class DivisibleBySeven {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i = 101; i < 200 ; i++) {
+			if(i % 7 == 0) {
+				sum += i;
+				System.out.println(i + " " + sum);
+			}			
+		}
+		
+		System.out.println("Sum of all number between 100 and 200 and divisible by 7 is: " + sum);
+
+	}
+
+}
