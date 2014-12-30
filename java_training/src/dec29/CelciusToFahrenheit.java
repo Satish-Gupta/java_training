0c541006e15ed90dcca1b976c8c7cd576f2b2a24
Satish <satis.kumar.gupta@gmail.com>
2014-12-30 16:25:54 +0545

    assign celcius unit string and fahrenheit string to constants

--- java_training/src/dec29/CelciusToFahrenheit.java
+++ java_training/src/dec29/CelciusToFahrenheit.java
@@ -5,17 +5,20 @@
 package dec29;
 
 /**
+ * This class converts degree celcius value from 0 to 100 to it fahrenheit equivalent.
+ * 
  * @author satish
  *
  */
 public class CelciusToFahrenheit {
 
 	public static void main(String[] args) {
-		
+		final String CELCIUS_UNIT = "\u2103";
+		final String FAHRENHEIT_UNIT = "\u2103";
 		for(int i = 0; i < 100; i++) {
 			double temparatureInCelcius = i;
 			double temparatureInFahrenheit = temparatureInCelcius * 9 / 5 + 32;
-			System.out.println(temparatureInCelcius + "\u2103" + temparatureInFahrenheit + "\u2109" );
+			System.out.println(temparatureInCelcius + CELCIUS_UNIT + " = " + temparatureInFahrenheit + FAHRENHEIT_UNIT );
 		}
 	}
 
