12f643019c1cd760ccafc043640e86de45875fa7
Satish <satis.kumar.gupta@gmail.com>
2014-12-30 17:34:19 +0545

    refactor

--- java_training/src/dec29/Calculator.java
+++ java_training/src/dec29/Calculator.java
@@ -13,19 +13,81 @@ import java.util.Scanner;
  *
  */
 public class Calculator {
+	final static String[] OPERATIONS = { "Addition", "Subtraction",
+			"Multiplication", "Division", "Modulus" };
+	final static String[] OPERATORS = { "+", "-", "*", "/", "%" };
+	public static String operator = "";
+	static double operand1 = 0;
+	static double operand2 = 0;
 
-	public static void main(String[] args) {
+	/*
+	 * @param operand1 first operand
+	 * 
+	 * @param operand2 second operand
+	 * 
+	 * @return result after add operation
+	 */
+	public static double add(double operand1, double operand2) {
+		return operand1 + operand2;
+	}
+	
+	/*
+	 * @param operand1 first operand
+	 * 
+	 * @param operand2 second operand
+	 * 
+	 * @return result after subtract operation
+	 */
+	public static double subtract(double operand1, double operand2) {
+		return operand1 - operand2;
+	}
+
+	/*
+	 * @param operand1 first operand
+	 * 
+	 * @param operand2 second operand
+	 * 
+	 * @return result after multiply operation
+	 */
+	public static double multiply(double operand1, double operand2) {
+		return operand1 * operand2;
+	}
+
+	/*
+	 * @param operand1 first operand
+	 * 
+	 * @param operand2 second operand
+	 * 
+	 * @return result after divide operation
+	 */
+	public static double divide(double operand1, double operand2) {
+		return operand1 / operand2;
+	}
+
+	/*
+	 * @param operand1 first operand
+	 * 
+	 * @param operand2 second operand
+	 * 
+	 * @return result after mod operation
+	 */
+	public static double getRemainder(double operand1, double operand2) {
+		return operand1 % operand2;
+	}
+
+	/*
+	 * take input from user for operand1,operand2 and operation to be performed
+	 * accept input only if the operands are numerical data and the operation selected
+	 * is a valid operation. asks the user for correct input if unexpected input provided.
+	 * 
+	 *   @throws InputMismatchException if the user enters non-numerical data for operands
+	 *   		 or invalid operation option. 
+	 */
+	public static void getInput() {
 		Scanner scanner = new Scanner(System.in);
-		String operator = "";
-		final String[] OPERATIONS = { "Addition", "Subtraction", "Multiplication",
-				"Division", "Modulus" };
-		final String[] OPERATORS = { "+", "-", "*", "/", "%" };
-		double operand1 = 0;
-		double operand2 = 0;
-		double result = 0;
 		int choice = -1;
 		boolean isChoiceValid = false;
-		
+
 		boolean isInputFormatCorrect = false;
 
 		System.out.println("Select the operation you want to perform:");
@@ -34,14 +96,13 @@ public class Calculator {
 					+ OPERATORS[i] + ")");
 		}
 
-		
 		try {
 			do {
 				try {
 					choice = scanner.nextInt();
 					isInputFormatCorrect = true;
 					isChoiceValid = choice >= 1 && choice <= 5;
-					if(!isChoiceValid) {
+					if (!isChoiceValid) {
 						System.out.println("Please select valid option");
 					}
 
@@ -54,11 +115,10 @@ public class Calculator {
 			} while (!isChoiceValid);
 
 			// get operator for the selected option
-			if(isChoiceValid) {
+			if (isChoiceValid) {
 				// get operator for the selected option
-				operator = OPERATORS[choice - 1];
+				Calculator.operator = Calculator.OPERATORS[choice - 1];
 			}
-			
 
 			// reset isCorrectInputFormat to reuse it for first number
 			isInputFormatCorrect = false;
@@ -66,7 +126,7 @@ public class Calculator {
 			System.out.println("Enter the first number");
 			do {
 				try {
-					operand1 = scanner.nextDouble();
+					Calculator.operand1 = scanner.nextDouble();
 					isInputFormatCorrect = true;
 				} catch (InputMismatchException e) {
 					System.out
@@ -82,7 +142,7 @@ public class Calculator {
 
 			do {
 				try {
-					operand2 = scanner.nextDouble();
+					Calculator.operand2 = scanner.nextDouble();
 					isInputFormatCorrect = true;
 				} catch (InputMismatchException e) {
 					System.out
@@ -94,23 +154,30 @@ public class Calculator {
 		} finally {
 			scanner.close();
 		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double result = 0;
+
+		Calculator.getInput();
 
 		switch (operator) {
 		case "+":
-			result = operand1 + operand2;
+			result = Calculator.add(operand1, operand2);
 			break;
 		case "-":
-			result = operand1 - operand2;
+			result = Calculator.subtract(operand1, operand2);
 			break;
 		case "*":
-			result = operand1 * operand2;
+			result = Calculator.multiply(operand1, operand2);
 			break;
 		case "/":
 			try {
 				if (operand2 == 0) {
 					throw new ArithmeticException("Division by zero");
 				}
-				result = operand1 / operand2;
+				result = Calculator.divide(operand1, operand2);
 			} catch (Exception e) {
 				System.out.println(e);
 				return;
@@ -119,15 +186,15 @@ public class Calculator {
 			}
 			break;
 		case "%":
-			result = operand1 % operand2;
+			result = Calculator.getRemainder(operand1, operand2);
 			break;
 		default:
 			System.out.println("Wrong Input");
 			break;
 		}
 
-		System.out.println(operand1 + " " + operator + " " + operand2 + " = "
-				+ result);
+		System.out.println(operand1 + " " + Calculator.operator + " "
+				+ operand2 + " = " + result);
 
 	}
 
