844820552592a7c0de8580c84f73de5aec20407a
satish <satishgupta@lftechnology.com>
2015-01-06 06:49:56 +0545

    add searching of a value in an array

---
+++ java_training/src/com/lftechnology/jan5/ArraySearch.java
@@ -0,0 +1,75 @@
+package com.lftechnology.jan5;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+/**
+ * Search for a specific value in an array.
+ * 
+ * @author satish
+ * 
+ */
+public class ArraySearch {
+	private static Scanner scanner = new Scanner(System.in);
+	private static Logger logger = Logger.getLogger("mylogger");
+	private static String[] items = null;
+	private static String key = "";
+
+	public static void main(String[] args) {
+		try {
+			getItems();
+
+			System.out.println("Enter the item to be searched");
+			key = scanner.next();
+			searchKey();
+		} finally {
+			scanner.close();
+		}
+	}
+
+	/**
+	 * reads the items and populates it to the items array
+	 */
+	private static void getItems() {
+		int numberOfItems = 0;
+		boolean isInputValid = true;
+
+		System.out.println("Enter the number of items to be stored");
+
+		do {
+			try {
+				numberOfItems = Math.abs(scanner.nextInt());
+				isInputValid = true;
+			} catch (InputMismatchException exception) {
+				isInputValid = false;
+				scanner.nextLine();
+				logger.warning("Wrong input. Please enter a number");
+			}
+		} while (!isInputValid);
+
+		items = new String[numberOfItems];
+
+		System.out.println("Enter the numbers");
+		for (int i = 0; i < numberOfItems; i++) {
+			items[i] = scanner.next();
+		}
+
+	}
+
+	/**
+	 * searches an array for a given key and displays the result
+	 */
+	private static void searchKey() {
+		boolean isFound = false;
+		for (int i = 0; i < items.length; i++) {
+			if (items[i].equalsIgnoreCase(key)) {
+				isFound = true;
+				logger.info("key: " + key + " found at:" + (i + 1));
+			}
+		}
+		if (!isFound) {
+			logger.info("key: " + key + " not found");
+		}
+	}
+}
