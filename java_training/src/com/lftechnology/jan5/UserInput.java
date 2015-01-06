3e879260cd2eb71756216a8c9a49ee35d98ebce0
satish <satishgupta@lftechnology.com>
2015-01-06 06:50:40 +0545

    add repeated userInput with quit option

---
+++ java_training/src/com/lftechnology/jan5/UserInput.java
@@ -0,0 +1,33 @@
+package com.lftechnology.jan5;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+/**
+ * Try to do a program that constantly prompts the user to enter something
+ * (using the Scanner class) until the user enters a specific word (such as quit
+ * for example)
+ * 
+ * @author satish
+ * 
+ */
+public class UserInput {
+	private static Scanner scanner = new Scanner(System.in);
+	private static Logger logger = Logger.getLogger("myLogger");
+
+	public static void main(String[] args) {
+		String userInput = "";
+		try {
+			do {
+				System.out.println("Enter something or enter quit to exit");
+				userInput = scanner.nextLine();
+				if (userInput.equalsIgnoreCase("quit")) {
+					break;
+				}
+				logger.info(userInput + " Entered");
+			} while (true);
+		} finally {
+			scanner.close();
+		}
+	}
+}
