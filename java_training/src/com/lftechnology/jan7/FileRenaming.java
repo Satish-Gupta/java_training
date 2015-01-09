02e43cf12e3c945540c3ed64feaeff8b3bc5ad3b
satish <satishgupta@lftechnology.com>
2015-01-09 09:43:11 +0545

    add renaming a file

--- java_training/src/com/lftechnology/jan7/FileRenaming.java
+++ java_training/src/com/lftechnology/jan7/FileRenaming.java
@@ -1,42 +1,45 @@
 package com.lftechnology.jan7;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * @author satish
  * 
  */
-public class FileRenameIO {
-	private static final Logger logger = Logger.getLogger(FileRenameNIO.class.getName());
+public class FileRenaming {
+	private static final Logger logger = Logger.getLogger(FileRenaming.class.getName());
 
 	public static void main(String[] args) {
-		final int ERROR_EXIT_CODE = 0;
-		boolean isRenamed = false;
+		final int EXIT_ERROR_CODE = 0;
 		if (args.length < 2) {
-			logger.severe("Usage:" + FileRenameNIO.class.getName() + " originalFile " + "renameFile");
-			System.exit(ERROR_EXIT_CODE);
+			logger.log(Level.SEVERE, "Usage: {0}, old_file new_file_name" + FileRenaming.class.getName());
+			System.exit(EXIT_ERROR_CODE);
 		}
+		boolean isFileRenamed = false;
 		String originalFilePath = args[0];
-		String newFileNameStr = args[1];
-		System.out.println(newFileNameStr);
+		String newFileName = args[1];
+
 		File originalFile = new File(originalFilePath);
-		File newFile = new File(newFileNameStr);
-
-		if (!originalFile.exists()) {
-			logger.severe("The given file does not exist");
-			System.exit(ERROR_EXIT_CODE);
+		File newFile = new File(newFileName);
+		try {
+			isFileRenamed = FileUtils.renameFile(originalFile, newFileName);
+		} catch (FileNotFoundException | FileAlreadyExistsException e) {
+			logger.log(Level.SEVERE, "{0}, at class {1}, cause:{2}", new Object[] { e.getMessage(), e.getClass(), e.getCause() });
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "{0}, at class {1}, cause:{2}", new Object[] { e.getMessage(), e.getClass(), e.getCause() });
 		}
-
-		logger.info("Before Renaming, FileName: " + originalFile.getName());
-		isRenamed = originalFile.renameTo(newFile);
-		if (isRenamed) {
-			logger.info("After renaming, FileName:" + newFile.getName());
-		} else {
-			logger.severe("file rename failed");
+		if (isFileRenamed) {
+			logger.log(Level.INFO, "File {0} Exists? {1} ", new Object[] { newFile.getAbsoluteFile(), newFile.exists() });
+			try {
+				FileUtils.deleteFile(newFile);
+			} catch (FileNotFoundException e) {
+				logger.log(Level.SEVERE, "{0}, at class {1}, cause:{2}", new Object[] { e.getMessage(), e.getClass(), e.getCause() });
+			}
+			logger.log(Level.INFO, "File {0} Exists? {1} ", new Object[] { newFile.getAbsoluteFile(), newFile.exists() });
 		}
-		newFile.delete();
-		logger.info("File Exists " + originalFile.exists());
 	}
-
 }
