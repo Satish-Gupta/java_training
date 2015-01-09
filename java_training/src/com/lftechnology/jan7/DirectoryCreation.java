359e46d6eb907dde9f6565eba81e388035155e66
satish <satishgupta@lftechnology.com>
2015-01-09 09:42:27 +0545

    add directory creation

--- java_training/src/com/lftechnology/jan7/DirectoryCreation.java
+++ java_training/src/com/lftechnology/jan7/DirectoryCreation.java
@@ -1,31 +1,26 @@
 package com.lftechnology.jan7;
 
 import java.io.File;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Write a program to create a directory and check whether the directory is created.
+ * 
  * @author satish
  * 
  */
-public class DirectoryCreationIO {
-	private static final Logger logger = Logger.getLogger(DirectoryCreationIO.class.getName());
+public class DirectoryCreation {
+	private static final Logger logger = Logger.getLogger(DirectoryCreation.class.getName());
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		File file = new File("/home/satish/java/myDirectory");
-		boolean isDirectoryCreated = false;
-		// get the index of the directory to be created;
 
-		if (file.exists()) {
-			logger.severe("The directory " + file.getName() + " already exists");
+		try {
+			FileUtils.createDirectory(file);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Exception:{0} class:{1}, cause:{2}", new Object[] { e.getMessage(), e.getClass(), e.getCause() });
 		}
-		isDirectoryCreated = file.mkdir();
-		if (isDirectoryCreated) {
-			logger.info("Directory " + file.getName() + " created");
-		} else {
-			logger.severe("Directory creation failed");
-		}
+
 	}
 }
