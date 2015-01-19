217f4d8843f6db6833d3fcff686e465c7d858c7c
satish <satishgupta@lftechnology.com>
2015-01-19 13:07:45 +0545

    create class representing an auto mobile Service Center that provides services to different Vehicles

---
+++ java_training/src/com/lftechnology/AutoServiceManagement/AutoMobileServiceConstants.java
@@ -0,0 +1,13 @@
+package com.lftechnology.AutoServiceManagement;
+
+/**
+ * This class hold common constants for the AutoMobileServiceMangament
+ * 
+ * @author satish<satishgupta@lftechnology.com>
+ * 
+ */
+final public class AutoMobileServiceConstants {
+	public static final VehicleService WASH = new VehicleService("wash", 200);
+	public static final VehicleService POLISH = new VehicleService("polish", 50);
+	public static final VehicleService PAINT = new VehicleService("paint", 5000);
+}
