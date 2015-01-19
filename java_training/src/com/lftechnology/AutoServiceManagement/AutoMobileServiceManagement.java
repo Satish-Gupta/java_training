1076e0dac01a7f06c54435477fcd5a81bb5fa022
satish <satishgupta@lftechnology.com>
2015-01-19 13:11:37 +0545

    move performing services for vehicles to its responsible class AutoCenter

--- java_training/src/com/lftechnology/AutoServiceManagement/AutoMobileServiceManagement.java
+++ java_training/src/com/lftechnology/AutoServiceManagement/AutoMobileServiceManagement.java
@@ -1,6 +1,7 @@
 package com.lftechnology.AutoServiceManagement;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,64 +17,25 @@ public class AutoMobileServiceManagement {
 	private final static Logger LOGGER = Logger.getLogger(AutoMobileServiceManagement.class.getName());
 
 	public static void main(String[] args) {
-		final VehicleService wash = new VehicleService("wash", 200);
-		final VehicleService wax = new VehicleService("wax", 50);
-		final VehicleService color = new VehicleService("color", 5000);
-		Car car = new Car(1111L);
-		List<VehicleService> servicesRequested = new ArrayList<VehicleService>();
+		List<VehicleService> services = new ArrayList<VehicleService>();
+		services.add(AutoMobileServiceConstants.WASH);
+		services.add(AutoMobileServiceConstants.POLISH);
+		services.add(AutoMobileServiceConstants.PAINT);
+		AutoCenter autoCenter = new AutoCenter("Mahalaxmi auto Center", services);
+		Vehicle vehicle = new Car(1111L);
 
-		LOGGER.log(Level.INFO, "requesting services wash and color for {0}", car);
-		// request services for the car
-		servicesRequested.add(wash);
-		servicesRequested.add(color);
+		LOGGER.log(Level.INFO, "requesting services wash and paint for {0}", vehicle);
+		// request service wash and paint and get the Bill for those requested Services
+		Bill bill =
+				autoCenter.requestService(vehicle,
+						new ArrayList<VehicleService>(Arrays.asList(AutoMobileServiceConstants.WASH, AutoMobileServiceConstants.PAINT)));
 
-		Bill bill = new Bill(servicesRequested);
-		bill.calculateTotal();
-
-		LOGGER.log(Level.INFO, "Total bill amount for the {0} is {1}", new Object[] { car, bill.getTotal() });
-		LOGGER.log(Level.INFO, "Performing requested Services for {0}", car);
-		for (VehicleService service : servicesRequested) {
-			if (service.equals(wash)) {
-				wash(car);
-			} else if (service.equals(wax)) {
-				polish(car);
-			} else {
-				paint(car);
-			}
-		}
-		LOGGER.log(Level.INFO, "servicing for {0} completed", car);
+		LOGGER.log(Level.INFO, "Total bill amount for the {0} is {1}", new Object[] { vehicle, bill.getTotal() });
+		LOGGER.log(Level.INFO, "Performing requested Services for {0}", vehicle);
+		autoCenter.processServiceQueue();
+		LOGGER.log(Level.INFO, "servicing for {0} completed", vehicle);
 		LOGGER.log(Level.INFO, "Clearing the bill");
 		bill.clear();
-		LOGGER.log(Level.INFO, "Bill Status of {0} : {1}", new Object[] { car, bill.isCleared() ? "cleared" : "pending" });
-	}
-
-	/**
-	 * Performs wash operation on object of {@link Car}
-	 * 
-	 * @param {@link Car} to be washed
-	 * @author satish<satishgupta@lftechnology.com>
-	 */
-	public static void wash(Car car) {
-		LOGGER.info("washing " + car);
-	}
-
-	/**
-	 * Performs polish operation on object of {@link Car}
-	 * 
-	 * @param {@link Car} to be polished with wax
-	 * @author satish<satishgupta@lftechnology.com>
-	 */
-	public static void polish(Car car) {
-		LOGGER.info("polishing " + car);
-	}
-
-	/**
-	 * Performs paint operation on object of {@link Car}
-	 * 
-	 * @param {@link Car} to be painted
-	 * @author satish<satishgupta@lftechnology.com>
-	 */
-	public static void paint(Car car) {
-		LOGGER.info("painting" + car);
+		LOGGER.log(Level.INFO, "Bill Status of {0} : {1}", new Object[] { vehicle, bill.isCleared() ? "cleared" : "pending" });
 	}
 }
